a5606b00486c64d23355598255aff5d8c1e80a35
thisLight <l1589002388@gmail.com>
2022-08-26 22:43:30 +0800

    oauth2: correct name for code_verifier

--- src/lib/oauth2.ts
+++ src/lib/oauth2.ts
@@ -1,3 +1,4 @@
+import { BadStateError } from "./errors"
 import { Oauth2Service, OpenAPI } from "./internal"
 import { tokenDetailByRefTok } from "./sessions"
 import {ClientConfig, Session} from "./types"
@@ -5,7 +6,7 @@ import {ClientConfig, Session} from "./types"
 export type OAuth2AuthorizationCodeFlowRequest = {
   readonly redirect_uri: string
   readonly scope: string
-  readonly code_challenge: string
+  readonly code_verifier: string
   readonly state?: string
   readonly ua_id?: string
 }
@@ -24,13 +25,21 @@ export function getAuthorizationUrlForAuthorizationCodeFlow(client: ClientConfig
   return url
 }
 
-export async function completeAuthorizationCodeFlow(client: ClientConfig, redirect_uri: string, code_challenge: string, code: string) {
+/** Complete OAuth 2 authorization code flow using authorization code.
+ *
+ * @param client client config
+ * @param redirect_uri the redirct URI used at the start of authorization
+ * @param code_verifier the code verifier string used
+ * @param code the authorization code
+ * @returns a session object
+ */
+export async function completeAuthorizationCodeFlow(client: ClientConfig, redirect_uri: string, code_verifier: string, code: string): Promise<Session> {
   OpenAPI.BASE = client.endpointBase
   const response = await Oauth2Service.oauth2ExchangeAccessTokenOauth2TokenPost({
     grant_type: "authorization_code",
     code,
     redirect_uri,
-    code_verifier: code_challenge,
+    code_verifier,
     client_id: client.clientId,
     client_secret: client.clientSecret ?? undefined,
   })

47a601d6c7653450779c1f0394b82dac25df5e3e
thisLight <l1589002388@gmail.com>
2022-08-26 21:03:52 +0800

    getAccessTokenByRefreshToken: correctly handle errors

--- src/lib/sessions.ts
+++ src/lib/sessions.ts
@@ -1,19 +1,31 @@
-import { OpenAPI, SessionsService } from "./internal";
+import { ApiError, OpenAPI, SessionsService } from "./internal";
 import { AccessToken, ClientConfig } from "./types";
 
 export async function getAccessTokenByRefreshToken(client: ClientConfig, refreshToken: string): Promise<AccessToken | null> {
   OpenAPI.BASE = client.endpointBase
-  const response = await SessionsService.getAccessTokenByRefreshTokenAccessTokensSpecificRefTokGet(refreshToken)
-  const remoteTokObj = response.access_token
-  return <AccessToken>{
-    userid: remoteTokObj.userid,
-    refreshToken: remoteTokObj.refresh_token,
-    expiredAt: Date.parse(remoteTokObj.expired_at),
-    createdAt: Date.parse(remoteTokObj.created_at),
-    active: remoteTokObj.active,
-    appid: remoteTokObj.appid,
-    scope: remoteTokObj.scope,
-    updatedAt: Date.parse(remoteTokObj.updated_at),
-    userAgent: remoteTokObj.user_agent,
+  try {
+    const response = await SessionsService.getAccessTokenByRefreshTokenAccessTokensSpecificRefTokGet(refreshToken)
+    const remoteTokObj = response.access_token
+    return <AccessToken>{
+      userid: remoteTokObj.userid,
+      refreshToken: remoteTokObj.refresh_token,
+      expiredAt: Date.parse(remoteTokObj.expired_at),
+      createdAt: Date.parse(remoteTokObj.created_at),
+      active: remoteTokObj.active,
+      appid: remoteTokObj.appid,
+      scope: remoteTokObj.scope,
+      updatedAt: Date.parse(remoteTokObj.updated_at),
+      userAgent: remoteTokObj.user_agent,
+    }
+  } catch (e: unknown) {
+    if (e instanceof ApiError) {
+      if (e.status == 404) {
+        return null
+      } else {
+        throw e
+      }
+    } else {
+      throw e
+    }
   }
 }

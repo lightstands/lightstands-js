34b94ae3c12ca3045be5c3d935d362fd8d97de91
thisLight <l1589002388@gmail.com>
2022-09-10 10:24:31 +0800

    users: fix getUserPrivateInfo do not take session

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,64 +1,72 @@
 import { ForbiddenError, NotFoundError, UnauthorizedError } from './errors';
 import { aeither, Fork } from './fpcore';
-import { OpenAPI, UsersService } from './internal';
-import { ClientConfig, PrivateUser, PublicUser } from './types';
-import { wrapOpenAPI } from './utils';
+import { UsersService } from './internal';
+import { ClientConfig, PrivateUser, PublicUser, Session } from './types';
+import { ensureOpenAPIEnv, wrapOpenAPI } from './utils';
 
 export function getUserPublicInfo(
   client: ClientConfig,
   userid: number,
 ): Fork<NotFoundError, PublicUser> {
-  OpenAPI.BASE = client.endpointBase;
-  return aeither(
-    {
-      left(l) {
-        if (l.status == 404) {
-          return new NotFoundError('userid');
-        } else {
-          throw l;
-        }
-      },
-      right(r): PublicUser {
-        return {
-          userid: r.userid,
-          username: r.username,
-        };
-      },
-    },
-    wrapOpenAPI(
-      UsersService.getUserPublicInformationUsersUseridPublicGet(userid),
-    ),
+  return ensureOpenAPIEnv(
+    () =>
+      aeither(
+        {
+          left(l) {
+            if (l.status == 404) {
+              return new NotFoundError('userid');
+            } else {
+              throw l;
+            }
+          },
+          right(r): PublicUser {
+            return {
+              userid: r.userid,
+              username: r.username,
+            };
+          },
+        },
+        wrapOpenAPI(
+          UsersService.getUserPublicInformationUsersUseridPublicGet(userid),
+        ),
+      ),
+    client,
   );
 }
 
 export function getUserPrivateInfo(
   client: ClientConfig,
+  session: Session,
   userid: number,
 ): Fork<NotFoundError | UnauthorizedError | ForbiddenError, PublicUser> {
-  OpenAPI.BASE = client.endpointBase;
-  return aeither(
-    {
-      left(l) {
-        if (l.status === 404) {
-          return new NotFoundError('userid');
-        } else if (l.status === 401) {
-          return new UnauthorizedError();
-        } else if (l.status === 403) {
-          return new ForbiddenError();
-        } else {
-          throw l;
-        }
-      },
-      right(r): PrivateUser {
-        return {
-          userid: r.userid,
-          username: r.username,
-          email: r.email,
-        };
-      },
-    },
-    wrapOpenAPI(
-      UsersService.getUserPrivateInformationUsersUseridPrivateGet(userid),
-    ),
+  return ensureOpenAPIEnv(
+    () =>
+      aeither(
+        {
+          left(l) {
+            if (l.status === 404) {
+              return new NotFoundError('userid');
+            } else if (l.status === 401) {
+              return new UnauthorizedError();
+            } else if (l.status === 403) {
+              return new ForbiddenError();
+            } else {
+              throw l;
+            }
+          },
+          right(r): PrivateUser {
+            return {
+              userid: r.userid,
+              username: r.username,
+              email: r.email,
+            };
+          },
+        },
+        wrapOpenAPI(
+          UsersService.getUserPrivateInformationUsersUseridPrivateGet(userid),
+        ),
+      ),
+    client,
+    session,
   );
 }

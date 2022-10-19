d701b6be37aa5c02bf4c9b83044dd8adf3c74a39
thisLight <l1589002388@gmail.com>
2022-10-19 13:10:09 +0800

    add euid library & remove hash-wasm

--- src/lib/crypto/blake3/index.ts
+++ src/lib/crypto/blake3/index.ts
@@ -1,4 +1,1 @@
-import { blake3 as blake3Js } from './js';
-import { blake3 as blake3Wasm } from './wasm';
-
-export const blake3 = typeof WebAssembly != 'undefined' ? blake3Wasm : blake3Js;
+export * from './js';
